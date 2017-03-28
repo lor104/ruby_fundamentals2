cb53fe45c375e59df3e467b75af753b063430798
lor104 <lauren.rosentzveig@gmail.com>
2017-03-28 19:34:37 -0400

    finish exercise 0

--- Collections and Iteration/exercise0.rb
+++ Collections and Iteration/exercise0.rb
@@ -4,28 +4,24 @@
 # Save each of the following arrays and hashes into variables. You will use them throughout the assignment.
 #
 # Eg. fav_colours = ...
-#
-# Create an array that contains the given information:
-#
-# your favourite colours
-# the age of you and your siblings/cousins/friends
-# flip a coin 5 times and record whether or not the result was 'heads'
-# your three favourite performing artists
-# your favourite colours again (this time as symbols instead of strings)
-# Create a hash that contains the given information:
-#
-# three words and their definitions
-# your favourite movie names and their year of creation
-# three cities of the world and their population
-# the names of your siblings/cousins/friends and their ages
 
+# your favourite colours
 favourite_colours = ["red", "blue", "rusty orange", "forest green", "black"]
+# the age of you and your siblings/cousins/friends
 sibling_age = [27, 22]
-coin_flip = ["tails", "heads", "heads", "heads", "heads"]
+# flip a coin 5 times and record whether or not the result was 'heads'
+coin_flip = [false, true, true, true, true]
+# your three favourite performing artists
 favourite_artist = ["Bruce Springsteen", "Leonard Cohen", "Cat Stevens"]
+# your favourite colours again (this time as symbols instead of strings)
 favourite_colours = [:red, :blue, :rusty_orange, :forest_green, :black]
 
+
+# three words and their definitions
 definitions = {:snap => "make a sudden, sharp, distinct, sound", :crackle => "to make slight, sudden, sharp noises, rapidly repeated", :pop => "to make a short, quick, explosive sound"}
+# your favourite movie names and their year of creation
 favourite_movies = {:cinderella => 1950, :snow_white => 1937, :sleeping_beauty => 1959}
+# three cities of the world and their population
 cities = {:shanghai => 24256800, :karachi => 23500000, :beijing => 21516000}
+# the names of your siblings/cousins/friends and their ages
 siblings = {:dara => 27, :jord => 22}
