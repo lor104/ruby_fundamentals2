367b5377b46cc56756653c48e78f960d3228577b
lor104 <lauren.rosentzveig@gmail.com>
2017-03-28 17:19:00 -0400

    comment throughout exercise 10 to make it more readable

--- exercise10.rb
+++ exercise10.rb
@@ -1,5 +1,7 @@
+#define hash
 students = {:cohort1 => 34, :cohort2 => 42, :cohort3 => 22}
 
+#displays the name and number of each cohort
 def print_hash(hash)
   hash.each do |key, value|
     puts "#{key}: #{value} students"
@@ -12,16 +14,20 @@ students[:cohort4] = 43
 
 print_hash(students)
 
+#use keys method to output all of the cohort names
 students.each { |key, value| puts "#{key}"}
 
+#expand classroom numbers by 5%
 students.each do |key, value|
   students[key] = value*1.05
   puts students[key]
 end
 
+#delete 2nd cohort
 students.delete(:cohort2)
 puts students
 
+#calculate total number of students across all cohorts
 total = 0
 students.each do |key, value|
   total = total + students[key]
