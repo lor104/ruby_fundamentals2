ae473e46db6a8573dee7e4592266608dc9ccc7ce
lor104 <lauren.rosentzveig@gmail.com>
2017-03-28 19:25:08 -0400

    create multiple arrays

---
+++ Collections and Iteration/exercise0.rb
@@ -0,0 +1,26 @@
+#INSTRUCTIONS
+# Creating Arrays and Hashes
+#
+# Save each of the following arrays and hashes into variables. You will use them throughout the assignment.
+#
+# Eg. fav_colours = ...
+#
+# Create an array that contains the given information:
+#
+# your favourite colours
+# the age of you and your siblings/cousins/friends
+# flip a coin 5 times and record whether or not the result was 'heads'
+# your three favourite performing artists
+# your favourite colours again (this time as symbols instead of strings)
+# Create a hash that contains the given information:
+#
+# three words and their definitions
+# your favourite movie names and their year of creation
+# three cities of the world and their population
+# the names of your siblings/cousins/friends and their ages
+
+favourite_colours = ["red", "blue", "rusty orange", "forest green", "black"]
+sibling_age = [27, 22]
+coin_flip = ["tails", "heads", "heads", "heads", "heads"]
+favourite_artist = ["Bruce Springsteen", "Leonard Cohen", "Cat Stevens"]
+favourite_colours = [:red, :blue, :rusty_orange, :forest_green, :black]
