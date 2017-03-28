cd42bff10a92b105c0bddddee5c325ca17a7a709
lor104 <lauren.rosentzveig@gmail.com>
2017-03-28 15:03:27 -0400

    write expenses sum code for exercise 8

---
+++ exercise8.rb
@@ -0,0 +1,16 @@
+#INSTRUCTIONS
+# You want to add up your expenses for the year. Create an array of numbers (integers or floats) that represents your expenses, eg:
+#
+# [250, 7.95, 30.95, 16.50]
+#
+# Add up the numbers and output the result.
+#
+# Tip: you may need a variable to keep track of the sum total. What value should it start at?
+#
+# Put this code into a method. The method should take an array as an argument and return the sum of the expenses in the array. Call the method twice with different arrays.
+
+expenses = [250, 7.95, 30.95, 16.50]
+sum = 0
+expenses.each do |num|
+  puts sum+=num
+end
